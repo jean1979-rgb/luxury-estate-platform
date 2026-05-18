4affe2fed3035761b94efe5a3c46b4d1e8616393
Christian Sanchez Jean <csanchezjean79@gmail.com>
2026-05-18 11:29:48 -0600

    feat: connect contact CTA to WhatsApp

--- components/ContactCTA.tsx
+++ components/ContactCTA.tsx
@@ -23,7 +23,7 @@ export default function ContactCTA({ title }: Props) {
 
         <div className="xl:col-span-5 xl:flex xl:justify-end">
           <a
-            href="https://wa.me/"
+            href="https://wa.me/527442250891?text=Hola,%20me%20interesa%20la%20propiedad"
             className="inline-flex min-h-[58px] w-full items-center justify-center rounded-full border border-[#d6c3a1] px-6 py-3 text-xs uppercase tracking-[0.28em] text-[#f5f1eb] transition hover:bg-[#d6c3a1] hover:text-black xl:w-auto xl:min-w-[220px]"
           >
             Contactar ahora
