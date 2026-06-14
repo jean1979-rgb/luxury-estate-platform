07b97bc6ee620cada6ee58aff85bc235437e01fe
Christian Sanchez Jean <csanchezjean79@gmail.com>
2026-06-14 16:09:02 -0600

    fix: keep admin route dynamic

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,7 +1,7 @@
 export const dynamic = "force-dynamic";
+
 import AdminClient from "./AdminClient";
 
-export const revalidate = 0;
 
 export default function AdminPage() {
   return <AdminClient />;
